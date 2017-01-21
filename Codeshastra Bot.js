958d17c6058b6d9a1f24e5cdc5461ab6031c71bd
Abhishek Shah <shahabhishek96@gmail.com>
2017-01-21 19:02:56 +0530

    Core Version

---
+++ Codeshastra Bot.js
@@ -0,0 +1,20 @@
+var http=require('http');
+var fs=require('fs');
+
+function send404(response){
+	response.writeHead(404, {"Context-Type": "text/plain"});
+	response.write("<h1>Error 404: Site Not Found!</h1>");
+	response.end();
+}
+
+function onRequest(request,response){
+	console.log('A request was made by the user '+ request.url);
+	if(request.method=='GET'&&request.url=='/'){
+		response.writeHead(200,{"Context-Type": "text/html"});
+		fs.createReadStream("./index.html").pipe(response);
+	}else{
+		send404(response);
+	}
+}
+http.createServer(onRequest).listen(8088);
+console.log("Server is Running");
